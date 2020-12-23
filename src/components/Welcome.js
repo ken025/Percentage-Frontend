be76788df71b40b99de2dd57011d346604a6c2ef
ken025 <kennethmanuel250@gmail.com>
2020-12-23 16:10:23 -0500

    Added welcome page to App.js

---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Welcome(){
+
+  return (
+    <>
+      <h2>PERCENTAGE!</h2>
+      <h3>Track your expenses and plan savings</h3>
+      <p>src - components - welcome.js </p>
+    </>
+  )
+}
